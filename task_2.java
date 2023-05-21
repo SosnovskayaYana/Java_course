7de55845ab5d6135d74056327067b4ce03c74b14
Yana <yan4ik999@yandex.ru>
2023-05-21 21:17:47 +0300

    task2_java_dz

---
+++ task_2.java
@@ -0,0 +1,26 @@
+/* Вывести все простые числа от 1 до 1000 */
+
+public class task_2 {
+
+    public static void main(String[] args) {
+
+        String prime = "";
+
+        for (int i = 2; i <= 1000; i++){
+            boolean primeNum = true;
+
+            for (int j = 2; j <i; j++){
+                if (i % j == 0){
+                    primeNum = false;
+                    break;
+                }
+            }
+            
+            if (primeNum){
+                prime = prime + i + ", ";
+            }
+        }
+        System.out.println("Простые числа от 1 до 1000: " + prime);
+
+    }
+}
