8237e4831441fb12ae88888b34c399d451f6be75
Yana <yan4ik999@yandex.ru>
2023-05-26 12:59:04 +0300

    dz3_java

--- DZ_java/dz_3_java/task_1.java
+++ DZ_java/dz_3_java/task_1.java
@@ -9,37 +9,100 @@
 package DZ_java.dz_3_java;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Random;
+// import java.util.Collections;
+
 
 public class task_1 {
     public static void main(String[] args) {
-        
+
         ArrayList<Integer> list = new ArrayList<Integer>();
 
-        for(int i = 0; i < 10; i ++){
+        for (int i = 0; i < 10; i++) {
             list.add(new Random().nextInt(20));
         }
         System.out.println(list);
 
+        System.out.println("Преобразованный список: " + sort(list));
+        System.out.println("Min значение: " + minValue(list));
+        System.out.println("Max значение: " + maxValue(list));
+        System.out.println("Среднее арифметическое: " + averagValue(list));
 
-        Iterator <Integer> i = list.iterator();
-        while (i.hasNext()){
+
+
+        // Iterator<Integer> i = list.iterator();
+        // while (i.hasNext()) {
+        // Integer s = i.next();
+        // if (s % 2 == 0) {
+        // i.remove();
+        // }
+        // }
+        // System.out.println("Преобразованный список: " + list);
+
+        // int min = Collections.min(list);
+        // System.out.printf("Min = %d,", min);
+        // int max = Collections.max(list);
+        // System.out.println("Max = " + max);
+
+    }
+
+
+
+    public static ArrayList<Integer> sort(ArrayList<Integer> list) {
+
+        Iterator<Integer> i = list.iterator();
+
+        while (i.hasNext()) {
             Integer s = i.next();
-            if (s % 2 == 0){
+            if (s % 2 == 0) {
                 i.remove();
             }
         }
-        System.out.println(list);
+        return list;
+    }
 
 
 
+    public static Integer minValue(List<Integer> list) {
+        int min = list.get(0);
 
-        int min = Collections.min(list);
-        System.out.printf("Min = %d,", min);
+        for (int i : list) {
+            if (i < min) {
+                min = i;
+            }
+        }
+        return min;
+    }
 
-        int max = Collections.max(list);
-        System.out.println("Max = " + max);
+
+
+    public static Integer maxValue(List<Integer> list) {
+        int max = list.get(0);
+
+        for (int i : list) {
+            if (i > max) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+
+
+    public static double averagValue(List<Integer> list) {
+        double sum = 0;
+
+        for (int i : list) {
+            sum = sum + i;
+        }
+
+        double average = (sum / list.size());
+        // способ округления числа до одного символа после запятой:
+        double scale = Math.pow(10, 1);
+        double result = Math.ceil(average * scale) / scale;
+
+        return result;
     }
 }
